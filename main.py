e12bc8b7209dc759b4e1743445bf97f36f4e3bb2
Imre <144881913+kkszki@users.noreply.github.com>
2025-09-06 13:40:13 +0200

    Update main.py
    Kiírjuk a kérést

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 
 app = Flask(__name__, template_folder='.', static_folder='.')
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        valasztasok = {
+            "fazis": request.form.get("fazis"),
+            }
+        print( f"Bejelölt opciók: {valasztasok}")
+    
     return render_template('index.html')
 
 
