e654a5ad109844df88ea9e0f5c89be4516fc761e
Imre <144881913+kkszki@users.noreply.github.com>
2025-09-06 16:18:55 +0200

    Update main.py
    opciók tömbbe tétele
    bejelöltek kiíratása

--- main.py
+++ main.py
@@ -1,15 +1,49 @@
-from flask import Flask, render_template, send_from_directory, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='.', static_folder='.')
 
+
+names = [
+    "konnektor",
+    "lampa",
+    "kapcsolo",
+    "amper",
+    "eloszto",
+    "villanytuzhely",
+    "bojler",
+    "fi-rele",
+    "foldeles",
+    "vezetekcsere",
+    "ujkiadas",
+    "terv",
+    "meres",
+    "ai-asszisztens",
+    "okos-kamera",
+    "telefonos-vezerles",
+    "automatizalas",
+    "okos-kapcsolo",
+    "szenzorok",
+    "helyiseg-vezerles",
+    "futes"
+]
+
+
 @app.route('/', methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
-        valasztasok = {
-            "fazis": request.form.get("fazis"),
-            }
-        print( f"Bejelölt opciók: {valasztasok}")
+    global names
     
+    if request.method == "POST":
+        selected_fazis = request.form.get('fazis')
+    if selected_fazis:
+        print(f"A kiválasztott fázis: {selected_fazis}")
+    else:
+        print("Nem lett kiválasztva fázis.")
+    for name in names:
+        if name in request.form:
+        # Csak kiírjuk a name-ot, ha be van pipálva
+            print (f"A bejelölt checkbox neve: {name}")
+    
+
     return render_template('index.html')
 
 
